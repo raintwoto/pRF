5cd45a7920670b230842ff7810bee1acadb133e9
raintwoto <raintwoto@gmail.com>
2018-02-06 12:40:29 +0100

    display

---
+++ stimulus_code/MRstim/MRstim/Displays/Prisma/displayParams.m
@@ -0,0 +1,15 @@
+function params = displayParams
+% Projector at Skyra
+% ARW 022402
+% Critical parameters
+params.numPixels = [1024 768];
+params.dimensions = [45.0, 34.1];
+params.distance = 90.5;
+params.frameRate = 60;
+params.cmapDepth = 10;
+params.screenNumber = 0;
+% Descriptive parameters
+params.computerName = 'Skyra';
+params.monitor = 'Eiki Projector';
+%params.card = 'RadeonMobility7500';
+%params.position = 'Jordan 474';
